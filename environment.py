67ea41d9df6ff8991158e72e803b25dd95515102
Marko Boben <mboben@abelium.eu>
2018-09-03 23:11:55 +0200

    Rewrite of graph changes

--- environment.py
+++ environment.py
@@ -86,17 +86,23 @@ class GraphEdge(object):
         self.to_node = to_node
 
 
+class NodeQueryType(Enum):
+    ANY_NODE = 1
+    NODE_WITH_ID = 2
+    NODE_WITH_ATTR = 3
+
+
 class EnvironmentGraph(object):
-    def __init__(self, dictionary):
-        self._nodes = []  # type: List[GraphNode]
+    def __init__(self, dictionary=None):
         self._edge_map = {}
         self._node_map = {}
         self._class_name_map = {}
-        self._from_dictionary(dictionary)
+        if dictionary is not None:
+            self._from_dictionary(dictionary)
 
     def _from_dictionary(self, d):
-        self._nodes = [GraphNode.from_dict(n) for n in d['nodes']]
-        for n in self._nodes:
+        nodes = [GraphNode.from_dict(n) for n in d['nodes']]
+        for n in nodes:
             self._node_map[n.id] = n
             self._class_name_map.setdefault(n.class_name, []).append(n)
         edges = [(ed['from_id'], Relation[ed['relation_type'].upper()], ed['to_id'])
@@ -106,22 +112,28 @@ class EnvironmentGraph(object):
             es[to_id] = self._node_map[to_id]
 
     def get_nodes(self):
-        return self._nodes
+        return self._node_map.values()
+
+    def get_node_map(self):
+        return self._node_map
 
     def get_nodes_by_attr(self, attr: str, value):
         if attr == 'class_name':
             for node in self._class_name_map.get(value, []):
                 yield node
         else:
-            for node in self._nodes:
+            for node in self._node_map.values():
                 if getattr(node, attr) == value:
                     yield node
 
-    def get_node(self, node):
-        return self._node_map.get(node.id, None)
+    def get_node(self, node_id: int):
+        return self._node_map.get(node_id, None)
 
     def get_nodes_from(self, from_node: Node, relation: Relation):
         return self._get_node_maps_from(from_node, relation).values()
+
+    def get_node_ids_from(self, from_node: Node, relation: Relation):
+        return self._get_node_maps_from(from_node, relation).keys()
 
     def _get_node_maps_from(self, from_node: Node, relation: Relation):
         return self._edge_map.get((from_node.id, relation), {})
@@ -129,57 +141,19 @@ class EnvironmentGraph(object):
     def has_edge(self, from_node: Node, relation: Relation, to_node: Node):
         return to_node.id in self._get_node_maps_from(from_node, relation)
 
+    def add_node(self, node: GraphNode):
+        assert node.id not in self._node_map
+        self._node_map[node.id] = node
+        self._class_name_map.setdefault(node.class_name, []).append(node)
 
-# EnvironmentState, state changes
+    def add_edge(self, from_node: Node, r: Relation, to_node: Node):
+        assert from_node.id in self._node_map and to_node.id in self._node_map
+        es = self._edge_map.setdefault((from_node.id, r), {})
+        es[to_node.id] = to_node
+
+
+# EnvironmentState
 ###############################################################################
-
-
-class ChangeType(Enum):
-    ADD_NODE = 0
-    DELETE_NODE = 1
-    ADD_EDGE = 2
-    DELETE_EDGE = 3
-
-
-class StateChange(object):
-
-    def __init__(self, change_type: ChangeType, change_object):
-        self.type = change_type
-        self.object = change_object
-
-    def is_add_node(self, node: Node):
-        return self.type == ChangeType.ADD_NODE and self.object.id == node.id
-
-    def of_add_node(self):
-        return self.object if self.type == ChangeType.ADD_NODE else None
-
-    def is_delete_node(self, node: Node):
-        return self.type == ChangeType.DELETE_NODE and self.object.id == node.id
-
-    def of_delete_node(self):
-        return self.object if self.type == ChangeType.DELETE_NODE else None
-
-    def is_add_edge(self, from_node: Node, relation: Relation, to_node: Node):
-        return (self.type == ChangeType.ADD_EDGE and self.object.from_node.id == from_node.id and
-                self.object.relation == relation and self.object.to_node.id == to_node.id)
-
-    def of_add_edge_from(self, from_node: Node, relation: Relation):
-        if (self.type == ChangeType.ADD_EDGE and self.object.from_node.id == from_node.id and
-                self.object.relation == relation):
-            return self.object.to_node
-        else:
-            return None
-
-    def is_delete_edge(self, from_node: Node, relation: Relation, to_node: Node):
-        return (self.type == ChangeType.DELETE_EDGE and self.object.from_node.id == from_node.id and
-                self.object.relation == relation and self.object.to_node.id == to_node.id)
-
-    def of_delete_edge_from(self, from_node: Node, relation: Relation):
-        if (self.type == ChangeType.DELETE_EDGE and self.object.from_node.id == from_node.id and
-                self.object.relation == relation):
-            return self.object.to_node
-        else:
-            return None
 
 
 class EnvironmentState(object):
@@ -187,7 +161,9 @@ class EnvironmentState(object):
     def __init__(self, graph: EnvironmentGraph):
         self._graph = graph
         self._script_objects = {}  # (name, instance) -> node id
-        self._changes = []  # type: List[StateChange]
+        self._new_nodes = {}  # map: node id -> GraphNode
+        self._removed_edges_from = {}  # map: (from_node id, relation) -> to_node id set
+        self._new_edges_from = {}  # map: (from_node id, relation) -> to_node id set
 
     def evaluate(self, lvalue: 'LogicalValue'):
         return lvalue.evaluate(self)
@@ -204,78 +180,87 @@ class EnvironmentState(object):
 
     def get_state_node(self, obj: ScriptObject):
         node_id = self._script_objects.get((obj.name, obj.instance), -1)
-        return None if node_id < 0 else self.get_node(Node(node_id))
+        return None if node_id < 0 else self.get_node(node_id)
 
     def has_edge(self, from_node: Node, relation: Relation, to_node: Node):
-        tm = TimeMeasurement.start('get_edge - changes')
-        for change in reversed(self._changes):
-            if change.is_delete_edge(from_node, relation, to_node):
-                return False
-            elif change.is_add_edge(from_node, relation, to_node):
-                return True
-        TimeMeasurement.stop(tm)
-        return self._graph.has_edge(from_node, relation, to_node)
+        if to_node.id in self._removed_edges_from.get((from_node.id, relation), set()):
+            return False
+        elif to_node.id in self._new_edges_from.get((from_node.id, relation), set()):
+            return True
+        else:
+            return self._graph.has_edge(from_node, relation, to_node)
 
-    def get_node(self, node: Node):
-        for change in reversed(self._changes):
-            if change.is_delete_node(node):
-                return None
-            elif change.is_add_node(node):
-                return change.object()
-        return self._graph.get_node(node)
+    def get_node(self, node_id: int):
+        if node_id in self._new_nodes:
+            return self._new_nodes[node_id]
+        else:
+            return self._graph.get_node(node_id)
 
-    def nodes_from(self, from_node: Node, relation: Relation):
-        deleted = set()  # deleted node ids
-        for change in reversed(self._changes):
-            to_node = change.of_delete_edge_from(from_node, relation)
-            if to_node:
-                deleted.add(to_node.id)
-                continue
-            to_node = change.of_add_edge_from(from_node, relation)
-            if to_node and to_node.id not in deleted:
-                yield to_node
-        for to_node in self._graph.get_nodes_from(from_node, relation):
-            if to_node.id not in deleted:
-                yield to_node
+    def get_nodes_from(self, from_node: Node, relation: Relation):
+        id_set = self._new_edges_from.get((from_node.id, relation), set())
+        id_set.update(self._graph.get_node_ids_from(from_node, relation))
+        removed_ids = self._removed_edges_from.get((from_node.id, relation), set())
+        id_set.difference_update(removed_ids)
+        result = []
+        for node_id in id_set:
+            if node_id in self._new_nodes:
+                result.append(self._new_nodes[node_id])
+            else:
+                result.append(self._graph.get_node(node_id))
+        return result
 
     def get_nodes(self):
-        deleted = set()  # deleted
-        for change in reversed(self._changes):
-            n = change.of_delete_node()
-            if n:
-                deleted.add(n.id)
-                continue
-            n = change.of_add_node()
-            if n and n.id not in deleted:
-                yield n
-        for n in self._graph.get_nodes():
-            if n.id not in deleted:
-                yield n
+        result = []
+        for node in self._graph.get_nodes():
+            if node.id in self._new_nodes:
+                result.append(self._new_nodes[node.id])
+            else:
+                result.append(node)
+        return result
 
     def get_nodes_by_attr(self, attr: str, value):
-        deleted = set()  # deleted
-        for change in reversed(self._changes):
-            n = change.of_delete_node()
-            if n:
-                deleted.add(n.id)
-                continue
-            n = change.of_add_node()
-            if n and n.id not in deleted and getattr(n, attr) == value:
-                yield n
-        for n in self._graph.get_nodes_by_attr(attr, value):
-            if n.id not in deleted:
-                yield n
+        result = []
+        added_new = set()
+        for node in self._graph.get_nodes_by_attr(attr, value):
+            if node.id not in self._new_nodes:
+                result.append(node)
+            else:
+                new_node = self._new_nodes[node.id]
+                if getattr(new_node, attr) == value:
+                    result.append(new_node)
+                    added_new.add(new_node.id)
+        for new_node_id, new_node in self._new_nodes.items():
+            if new_node_id not in added_new and getattr(new_node, attr) == value:
+                result.append(new_node)
+        return result
+
+    def add_edge(self, from_node: Node, relation: Relation, to_node: Node):
+        if (from_node.id, relation) in self._removed_edges_from:
+            to_node_ids = self._removed_edges_from[(from_node.id, relation)]
+            if to_node.id in to_node_ids:
+                to_node_ids.remove(to_node.id)
+        elif not self._graph.has_edge(from_node, relation, to_node):
+            self._new_edges_from.setdefault((from_node.id, relation), set()).add(to_node.id)
+
+    def delete_edge(self, from_node: Node, relation: Relation, to_node: Node):
+        if self._graph.has_edge(from_node, relation, to_node):
+            self._removed_edges_from.setdefault((from_node.id, relation), set()).add(to_node.id)
+        elif (from_node.id, relation) in self._new_edges_from:
+            to_node_ids = self._new_edges_from[(from_node.id, relation)]
+            if to_node.id in to_node_ids:
+                to_node_ids.remove(to_node.id)
 
     def change_state(self, changers: List['StateChanger'], node: Node, obj: ScriptObject = None):
         tm = TimeMeasurement.start('change_state')
         new_state = EnvironmentState(self._graph)
-        new_state._changes = self._changes.copy()
+        new_state._new_nodes = self._new_nodes.copy()
+        new_state._removed_edges_from = self._removed_edges_from.copy()
+        new_state._new_edges_from = self._new_edges_from.copy()
         new_state._script_objects = self._script_objects.copy()
         if obj is not None:
             new_state._script_objects[(obj.name, obj.instance)] = node.id
         for changer in changers:
-            for change in changer.enumerate_changes(self):
-                new_state._changes.append(change)
+            changer.apply_changes(self)
         TimeMeasurement.stop(tm)
         return new_state
 
@@ -303,7 +288,7 @@ class NodeInstance(NodeEnumerator):
         self.node = node
 
     def enumerate(self, state: EnvironmentState):
-        yield state.get_node(self.node)
+        yield state.get_node(self.node.id)
 
 
 class RelationFrom(NodeEnumerator):
@@ -313,7 +298,7 @@ class RelationFrom(NodeEnumerator):
         self.relation = relation
 
     def enumerate(self, state: EnvironmentState):
-        return state.nodes_from(self.from_node, self.relation)
+        return state.get_nodes_from(self.from_node, self.relation)
 
 
 class CharacterNode(NodeEnumerator):
@@ -328,7 +313,7 @@ class RoomNode(NodeEnumerator):
         self.node = node
 
     def enumerate(self, state: EnvironmentState):
-        for n in state.nodes_from(self.node, Relation.INSIDE):
+        for n in state.get_nodes_from(self.node, Relation.INSIDE):
             if n.category == 'Rooms':
                 yield n
 
@@ -370,7 +355,7 @@ class NodeState(LogicalValue):
         self.node_state = node_state
 
     def evaluate(self, state: EnvironmentState):
-        gn = state.get_node(self.node)
+        gn = state.get_node(self.node.id)
         return False if gn is None else self.node_state in gn.state
 
 
@@ -381,7 +366,7 @@ class NodeProperty(LogicalValue):
         self.node_property = node_property
 
     def evaluate(self, state: EnvironmentState):
-        gn = state.get_node(self.node)
+        gn = state.get_node(self.node.id)
         return False if gn is None else self.node_property in gn.properties
 
 
@@ -407,7 +392,7 @@ class ExistsRelation(LogicalValue):
 class StateChanger(object):
 
     @abstractmethod
-    def enumerate_changes(self, state: EnvironmentState):
+    def apply_changes(self, state: EnvironmentState):
         pass
 
 
@@ -418,11 +403,11 @@ class AddEdges(StateChanger):
         self.relation = relation
         self.to_node = to_node
 
-    def enumerate_changes(self, state: EnvironmentState):
+    def apply_changes(self, state: EnvironmentState):
         tm = TimeMeasurement.start('AddEdges')
         for n1 in self.from_node.enumerate(state):
             for n2 in self.to_node.enumerate(state):
-                yield StateChange(ChangeType.ADD_EDGE, GraphEdge(n1, self.relation, n2))
+                state.add_edge(n1, self.relation, n2)
         TimeMeasurement.stop(tm)
 
 
@@ -433,10 +418,10 @@ class DeleteEdges(StateChanger):
         self.relations = relations
         self.to_node = to_node
 
-    def enumerate_changes(self, state: EnvironmentState):
+    def apply_changes(self, state: EnvironmentState):
         tm = TimeMeasurement.start('DeleteEdges')
         for n1 in self.from_node.enumerate(state):
             for e in self.relations:
                 for n2 in self.to_node.enumerate(state):
-                    yield StateChange(ChangeType.DELETE_EDGE, GraphEdge(n1, e, n2))
+                    state.delete_edge(n1, e, n2)
         TimeMeasurement.stop(tm)
