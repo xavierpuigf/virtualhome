4b080778c4c2cdc35d4004f60458ad58e1952d9e
Marko Boben <mboben@abelium.eu>
2018-09-21 10:02:39 -0400

    Resource builder now builds object_placing.json

--- resource_builder.py
+++ resource_builder.py
@@ -26,5 +26,39 @@ def transform_name_equivalence():
         f.write(json.dumps(result))
 
 
+def transform_object_placing():
+    begin_matcher = re.compile(r'BEGIN<([\w_\s]+)>')
+    end_matcher = re.compile(r'END<([\w_\s]+)>')
+    value_matcher = re.compile(r'([\w_\s]+)[\s]*\[([\w_\s]+)\]')
+
+    result = {}
+
+    with open('unity_resources/placingobjects.txt') as f:
+        key = None
+        value = None
+        for line in f:
+            m = begin_matcher.match(line)
+            if m:
+                key = m.group(1).lower().strip()
+            elif end_matcher.match(line):
+                key = None
+            elif key is not None:
+                value = line.lower().strip()
+                room = None
+                vm = value_matcher.match(value)
+                if vm:
+                    value = vm.group(1).strip()
+                    room = vm.group(2).strip()
+                    if len(room) == 0:
+                        room = None
+                if len(value) > 0:
+                    placings = result.setdefault(key, [])
+                    placings.append({'destination': value, 'relation': 'ON', 'room': room})
+
+    with open('resources/object_placing.json', 'w') as f:
+        f.write(json.dumps(result))
+
+
 if __name__ == '__main__':
-    transform_name_equivalence()
+    # transform_name_equivalence()
+    transform_object_placing()
