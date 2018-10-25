59a10c5dae036a79a47e4735334b644f67ea0031
xavierpuigf <xavierpuigf@gmail.com>
2018-10-25 11:29:52 -0400

    Modified viz of augment dataset

--- dataset_augmentation/main.js
+++ dataset_augmentation/main.js
@@ -1,4 +1,29 @@
+var exec_id = 0;
 
+/*function getTable(data_activities){
+    var activity_cont = [];
+    var acti = Object.keys(data_activities);
+    for (var it = 0; it < acti.length; it ++){
+        activity_cont.push([-data_activities[acti[it]], acti[it]])
+    }
+    activity_cont.sort();
+    
+
+}*/
+function toggleExec(){
+    exec_id = 1-exec_id;
+    if (exec_id == 0){
+        $("#toggleExec").attr('value', 'View exec');
+        $("#title").text('Stats all augmented programs');
+            $(".row_normal").show();
+    } 
+    else {
+            $("#toggleExec").attr('value', 'View all');
+            $("#title").text('Stats executable augmented programs');
+            $(".row_normal").hide();
+    }  
+    updateTable();
+}
 function obtainProg(prog_lines, indices){
     prog_str = '<table>';
     var indices_bin = []
@@ -20,6 +45,7 @@ function obtainProg(prog_lines, indices){
 function displayProg(it, prog_type){
     var progs = Object.keys(data);
     var res = data[progs[it]][2][prog_type];
+    
     if (res.length == 0){
         $('#cell'+it+'_'+prog_type).empty()
         $('#cell'+it+'_'+prog_type).append("No program");
@@ -54,7 +80,7 @@ function prevProg(it, prog_type){
         current_display[it][prog_type] -= 1;
         displayProg(it, prog_type)
     }
-
+    
 }
 function nextProg(it, prog_type){
     var progs = Object.keys(data);
@@ -64,42 +90,89 @@ function nextProg(it, prog_type){
         displayProg(it, prog_type)
     }
 }
+function updateTable(){
+    $("#stats").empty();
+    var table_str = "<h3> Number of programs </h3><table>";
+    table_str += "<tr><th> Original </th><th>Location </th><th>Affordance </th><th> Exception </th><th> All exceptions</th></th></tr>";
+    table_str += "<tr><td>"+stats['all_progs']['initial'][exec_id]+"</td><td>"+stats['all_progs']['location'][exec_id]+"</td><td>"+stats['all_progs']['affordance'][exec_id]+"</td><td>"+stats['all_progs']['program_exception'][exec_id]+"</td><td>"+stats['all_progs']['total'][exec_id]+"</td></tr>"
+    table_str += "<tr>";
+    for (var iti = 0; iti < 5; iti++){
+        var names = ['initial', 'location', 'affordance', 'program_exception', 'total']
+        type_exec = '_all';
+        if (exec_id == 1){
+            type_exec = '_exec';
+        }
+        img_name = 'viz/distr_' + names[iti] + type_exec + '.png';
+        table_str += '<td><img style="width:250px" src="'+img_name+'"></img></td>'
+    }
+    table_str += "</tr>"
+    table_str += "<tr>";
+    for (var iti = 0; iti < 5; iti++){
+        var names = ['initial', 'location', 'affordance', 'program_exception', 'total']
+        type_exec = '_all';
+        if (exec_id == 1){
+            type_exec = '_exec';
+        }
+        img_name = 'viz/len_' + names[iti] + type_exec + '.png';
+        table_str += '<td><img style="width:250px" src="'+img_name+'"></img></td>'
+    }
+    table_str += "</tr>"
+    table_str += "<tr>";
+    for (var iti = 0; iti < 5; iti++){
+        if (iti == 0 || iti == 4){
+            table_str += '<td></td>'
+        }
+        else {
+           var names = ['initial', 'location', 'affordance', 'program_exception', 'total']
+            type_exec = '_all';
+            if (exec_id == 1){
+                type_exec = '_exec';
+            }
+            img_name = 'viz/LCS_' + names[iti] + type_exec + '.png';
+            table_str += '<td><img style="width:250px" src="'+img_name+'"></img></td>' 
+        }
+        
+    }
+    table_str += "</tr>"   
+    table_str += "</table>";
+    $("#stats").append(table_str);
+}
 var current_display = [];
 $( document ).ready(function() {
         programs = Object.keys(data);
-        var table_str = "<h3> Number of programs </h3><table>";
-        table_str += "<tr><th> Original </th><th> Affordance </th><th> Place </th><th> Exception </th><th> All exceptions</th></th></tr>";
-        table_str += "<tr><td>"+stats['all_progs'][0]+"</td><td>"+stats['all_progs'][1]+"</td><td>"+stats['all_progs'][2]+"</td><td>"+stats['all_progs'][3]+"</td><td>"+stats['all_progs'][4]+"</td></tr>"
-        table_str += "</table>";
-        $("#stats").append(table_str)
-        for (var it = 0; it < programs.length; it++){
+        updateTable();
+        for (var it = 0; it < programs.length;  it++){
             element = data[programs[it]];
             var title_str = element[0];
             prog_str = obtainProg(element[1], []);
-            current_display.push([0,0,0]);
-            var str1 = '<button onclick="prevProg('+it+', 0)"> Prev </button>'
-            var str2 = '<button onclick="prevProg('+it+', 1)"> Prev </button>'
-            var str3 = '<button onclick="prevProg('+it+', 2)"> Prev </button>'
+            current_display.push({'location': 0, 'affordance':0, 'program_exception':0});
 
-            var str12 = '<button onclick="nextProg('+it+', 0)"> Next </button>'
-            var str22 = '<button onclick="nextProg('+it+', 1)"> Next </button>'
-            var str32 = '<button onclick="nextProg('+it+', 2)"> Next </button>'
-
-            var str1f = '<button onclick="firstProg('+it+', 0)"> First </button>'
-            var str2f = '<button onclick="firstProg('+it+', 1)"> First </button>'
-            var str3f = '<button onclick="firstProg('+it+', 2)"> First </button>'
-
-            var str1l = '<button onclick="lastProg('+it+', 0)"> Last </button>'
-            var str2l = '<button onclick="lastProg('+it+', 1)"> Last </button>'
-            var str3l = '<button onclick="lastProg('+it+', 2)"> Last </button>'
-            console.log(title_str);
+            var augment = ['location', 'affordance', 'program_exception'];
+            var buttons = []
+            for (var au_id = 0; au_id < augment.length; au_id++){
+                if (element[2][augment[au_id]].length > 0){
+                    var str1 = '<button onclick="prevProg('+it+', \''+augment[au_id]+'\')"> Prev </button>';
+                    var str2 = '<button onclick="nextProg('+it+', \''+augment[au_id]+'\')"> Next </button>';
+                    var str3 = '<button onclick="firstProg('+it+', \''+augment[au_id]+'\')"> First </button>'
+                    var str4 = '<button onclick="lastProg('+it+', \''+augment[au_id]+'\')"> Last </button>'
+                    buttons.push(str1+str2+str3+str4)
+                }
+                else {
+                    buttons.push('');
+                }
+                
+            }
+            class_name = 'row_normal'
+            if (element[3] == true){
+                class_name = 'row_exec';
+            }
             $('#table_res').append(
-                '<tr><br><br><td><h4>'+title_str+'</h4>' + prog_str + '</td>' +
-                '<td><br><br><br>'+str1+str12+str1f+str1l+'<div id="cnt'+it+'_'+0+'"></div><div id="cell'+it+'_'+0+'"></div></td>' + 
-                '<td><br><br><br>'+str2+str22+str2f+str2l+'<div id="cnt'+it+'_'+1+'"></div><div id="cell'+it+'_'+1+'"></div></td>' + 
-                '<td><br><br><br>'+str3+str32+str3f+str3l+'<div id="cnt'+it+'_'+2+'"></div><div id="cell'+it+'_'+2+'"></div></td>')
-            displayProg(it, 0);
-            displayProg(it, 1);
-            displayProg(it, 2);
+                '<tr class="'+class_name+'"><br><br><td><h4>'+title_str+'</h4>' + prog_str + '</td>' +
+                '<td><br><br><br>'+buttons[0]+'<div id="cnt'+it+'_'+0+'"></div><div id="cell'+it+'_'+augment[0]+'"></div></td>' + 
+                '<td><br><br><br>'+buttons[1]+'<div id="cnt'+it+'_'+1+'"></div><div id="cell'+it+'_'+augment[1]+'"></div></td>' + 
+                '<td><br><br><br>'+buttons[2]+'<div id="cnt'+it+'_'+2+'"></div><div id="cell'+it+'_'+augment[2]+'"></div></td>')
+            displayProg(it, augment[0]);
+            displayProg(it, augment[1]);
+            displayProg(it, augment[2]);
         }  
  });
