43dae30e5740fd1768130a5e06b756a5462c3895
xavierpuigf <xavierpuigf@gmail.com>
2019-03-06 00:03:37 -0500

    correct dataset exceptions

--- dataset_augmentation/augment_dataset_exceptions.py
+++ dataset_augmentation/augment_dataset_exceptions.py
@@ -187,7 +187,7 @@ def augment_dataset(d, programs):
             program_acum = []
             while not executable and max_iter < maximum_iters and lines_program is not None:        
                 (message, final_state, graph_state_list, input_graph, 
-                id_mapping, info, graph_helper) = check_programs.check_script(
+                id_mapping, info, graph_helper, modified_script) = check_programs.check_script(
                         lines_program, 
                         init_state, 
                         '../example_graphs/{}.json'.format(apt_name),
@@ -218,22 +218,21 @@ def augment_dataset(d, programs):
 
             # Save the program
             if executable and max_iter > 0:
-                lines_program_newinst = lines_program[:4]
-                for lp in lines_program[4:]:
-                    action, objects, indx = augmentation_utils.parseStrBlock(lp.strip())
-                    instruction = '[{}]'.format(action)
-                    for obj, idis in zip(objects, indx):
-                        idi = int(idis)
-                        obj_name = obj
-                        if (obj_name in graph_helper.possible_rooms and 
-                            (obj_name, idi) not in id_mapping):
-                            obj_name = graph_helper.equivalent_rooms[obj_name]
-                        try:
-                            graphid = id_mapping[(obj_name, idi)]
-                        except:
-                            print(program_name, lp)
-                        instruction += ' <{}> ({}.{})'.format(obj_name, idi, graphid)
-                    lines_program_newinst.append(instruction)
+                lines_program_newinst = []
+                for script_line in modified_script:
+                    script_line_str = '[{}]'.format(script_line.action.name)
+                    if script_line.object():
+                        script_line_str += ' <{}> ({})'.format(script_line.object().name, script_line.object().instance)
+                    if script_line.subject():
+                        script_line_str += ' <{}> ({})'.format(script_line.subject().name, script_line.subject().instance)
+
+                    for k, v in id_mapping.items():
+                        obj_name, obj_number = k
+                        id = v
+                        script_line_str = script_line_str.replace('<{}> ({})'.format(obj_name, id), 
+                                                                  '<{}> ({}.{})'.format(obj_name, obj_number, id))
+
+                    lines_program_newinst.append(script_line_str)
                 
                 augmented_progs_i_new_inst.append(lines_program_newinst)
                 augmented_preconds_i.append(init_state)
