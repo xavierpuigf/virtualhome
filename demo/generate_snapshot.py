abca58b0f218e0ea16edb0a8484949d680653b8a
Xavier Puig <xavierpuig@gmail.com>
2019-03-22 18:37:54 -0400

    refactor + added examples

---
+++ demo/generate_snapshot.py
@@ -0,0 +1,73 @@
+# Generate snapshot for a program. Make sure you have the executable open
+import json
+import sys
+import numpy as np
+import random
+import cv2
+
+sys.path.append('../simulation')
+sys.path.append('../dataset_augmentation/')
+
+from tqdm import tqdm
+from unity_simulator.comm_unity import UnityCommunication
+import add_preconds
+import evolving_graph.check_programs as check_programs
+
+# Add here your script
+script = ['[Walk] <television> (1)', '[SwitchOn] <television> (1)', 
+          '[Walk] <sofa> (1)', '[Find] <controller> (1)',
+          '[Grab] <controller> (1)']
+
+graph_input = '../example_graphs/TrimmedTestScene1_graph.json'
+
+cameras_ids = [-6, -5, -1]
+
+def build_grid_images(images):
+    image_steps = []
+    for image_step in images:
+        img_step_cameras = np.concatenate(image_step, 1)
+        image_steps.append(img_step_cameras)
+    final_image = np.concatenate(image_steps, 0)
+    return final_image
+
+def obtain_snapshots(graph_state_list, reference_graph, comm):
+    _, ncameras = comm.camera_count()
+    cameras_select = list(range(ncameras))
+    cameras_select = [cameras_select[x] for x in cameras_ids]
+    
+    seed = random.randint(1,100)
+    messages_expand, images = [], []
+    for graph_state in tqdm(graph_state_list):
+        comm.reset(0)
+        message = comm.expand_scene(graph_state, randomize=True, random_seed=seed)
+        messages_expand.append(message)
+        ok, imgs = comm.camera_image(cameras_select, mode='normal', image_height=480,  image_width=640)
+        images.append(imgs)
+
+    return messages_expand, images
+
+
+comm = UnityCommunication()
+
+print('Inferring preconditions...')
+preconds = add_preconds.get_preconds_script(script).printCondsJSON()
+print(preconds)
+
+print('Loading graph')
+with open(graph_input, 'r') as f:
+    graph_input = json.load(f)
+
+print('Executing script')
+info = check_programs.check_script(
+        script, preconds, graph_path=None, inp_graph_dict=graph_input)
+
+message, final_state, graph_state_list, graph_dict, id_mapping, info, helper, modif_script = info
+success = (message == 'Script is executable')
+
+if success:
+    print('Generating snapshots')
+    messages, images = obtain_snapshots(graph_state_list, graph_input, comm)
+    grid_img = build_grid_images(images)
+    cv2.imwrite('snapshot_test.png', grid_img)
+    print('Snapshot saved in demo/snapshot_test.png')
+    
---
+++ demo/generate_video.py
@@ -0,0 +1,12 @@
+# Generate video for a program. Make sure you have the executable open
+import sys
+sys.path.append('../simulation/')
+from unity_simulator_simulator.comm_unity import UnityCommunication
+script = ['[Walk] <sofa> (1)', '[Sit] <sofa> (1)'] # Add here your script
+print('Starting Unity...')
+comm = UnityCommunication()
+print('Starting scene...')
+comm.reset()
+print('Generating video...')
+comm.render_script(script, capture_screenshot=True)
+print('Generated, find video in simulation/unity_simulator/output/')
