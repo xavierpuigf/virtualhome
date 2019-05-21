f2e9b47bc21a36fb5a84a23b9e050dcfbbc08a60
xavierpuigf <xavierpuigf@gmail.com>
2019-05-21 16:29:53 -0400

    correct bug

--- simulation/evolving_graph/utils.py
+++ simulation/evolving_graph/utils.py
@@ -164,11 +164,11 @@ class graph_dict_helper(object):
 
     def __init__(self, properties_data=None, object_placing=None, object_states=None, max_nodes=300):
         if properties_data is None:
-            properties_data = utils.load_properties_data()
+            properties_data = load_properties_data()
         if object_placing is None:
-            object_placing = utils.load_object_placing()
+            object_placing = load_object_placing()
         if object_states is None:
-            object_states = utils.load_object_states()
+            object_states = load_object_states()
 
         self.properties_data = properties_data
         self.object_placing = object_placing
