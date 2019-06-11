b50b081d4dfd41b1734a186077a1c678c329de7a
Xavier Puig <xavierpuigf@gmail.com>
2019-06-11 07:55:58 -0400

    Update run_sim.sh

--- docker/run_sim.sh
+++ docker/run_sim.sh
@@ -1,5 +1,5 @@
 # 640 480 Beautiful
 xvfb-run --auto-servernum --server-args="-screen 0 624x324x24" \
-    unity_vol/executable_unix/synvid.x86_64 -batchmode \
+    unity_vol/executable_unix/exec_linux.x86_64 -batchmode \
     -screen-width=$1 -screen-height=$2 -screen-quality=$3 \
     -http-port=8080
