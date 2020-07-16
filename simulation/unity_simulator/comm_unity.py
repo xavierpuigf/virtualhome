2a3e063dccef49b5fcdc6ac46d3922917971d886
Xavier Puig <xavierpuigf@gmail.com>
2020-07-16 11:24:53 -0400

    changes api

--- simulation/unity_simulator/comm_unity.py
+++ simulation/unity_simulator/comm_unity.py
@@ -233,7 +233,7 @@ class UnityCommunication(object):
                       skip_execution=False, find_solution=False, output_folder='Output/', file_name_prefix="script",
                       frame_rate=5, image_synthesis=['normal'], save_pose_data=False,
                       image_width=640, image_height=480, gen_vid=True, recording=False,
-                      save_scene_states=False, character_resource='Chars/Male1', camera_mode=['AUTO'], time_scale=1.0, smooth_walk=False):
+                      save_scene_states=False, character_resource='Chars/Male1', camera_mode=['AUTO'], time_scale=1.0, skip_animation=False):
         """
         :param script: a list of script lines
         :param randomize_execution: randomly choose elements
@@ -263,7 +263,7 @@ class UnityCommunication(object):
                   'save_pose_data': save_pose_data, 'save_scene_states': save_scene_states,
                   'character_resource': character_resource, 'camera_mode': camera_mode, 'recording': recording,
                   'image_width': image_width, 'image_height': image_height,
-                  'time_scale': time_scale, 'smooth_walk': smooth_walk}
+                  'time_scale': time_scale, 'skip_animation': skip_animation}
         response = self.post_command({'id': str(time.time()), 'action': 'render_script',
                                       'stringParams': [json.dumps(params)] + script})
         if response['success']:
